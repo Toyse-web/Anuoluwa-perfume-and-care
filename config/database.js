4c7e6d8560910234e95537cc88653b45a72f7a41
Toyse_Devs <olayonwatoyib05@gmail.com>
2026-01-10 00:50:42 +0100

    Forcing to change to IP4

--- config/database.js
+++ config/database.js
@@ -3,7 +3,8 @@ const {Pool} = require("pg");
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL || "postgres://postgres:Jeanie1234*@localhost:5432/Anuoluwa-Store",
-    ssl: false
+    ssl: false,
+    family: 4
 });
 
 // Test connection
